a6cdf0d884d246a361f31c1ab80db9855ec1f9dc
Colin Waddell <mrcolin@gmail.com>
2017-03-11 13:24:12 +0000

    local mailing list plugin

--- templates/signup.php
+++ templates/signup.php
@@ -1,24 +1,26 @@
 <div class="row">
-  <!-- Begin MailChimp Signup Form -->
-  <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-  <style>
-    #mc_embed_signup input.email{
-      height: 38px;
-    }
-  </style>
-  <div id="mc_embed_signup">
-  <form action="//gays-okay.us15.list-manage.com/subscribe/post?u=597adc75bab54c49d3bc0d9e0&amp;id=3d3cd9726a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <div id="mc_embed_signup_scroll">
-        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="EMAIL ADDRESS" required>
-      
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true">
-          <input type="text" name="b_597adc75bab54c49d3bc0d9e0_3d3cd9726a" tabindex="-1" value="">
-        </div>
-
-        <div class="clear"><input type="submit" value="FIND OUT MORE" name="subscribe" id="mc-embedded-subscribe" class="btn btn-danger"></div>
-      </div>
-  </form>
+  <div class="col-lg-4">
+    <?php
+      $args = array(
+      'prepend' => '', 
+      'showname' => false,
+      'emailtxt' => '',
+      'emailholder' => 'Email Address...', 
+      'showsubmit' => true, 
+      'submittxt' => 'Find Out More', 
+      'jsthanks' => false,
+      'thankyou' => "Thanks, we'll email you when preorders are open"
+      );
+      echo smlsubform($args);
+    ?>
   </div>
-<!--End mc_embed_signup-->
 </div>
+
+<script>
+  jQuery('input[name="submit"]').addClass('btn btn-warning');
+  jQuery('input[name="sml_email"]').addClass('input form-control');
+  if (jQuery('.sml_thankyou').length){
+    jQuery('input[name="submit"]').hide();
+    jQuery('input[name="sml_email"]').hide();
+  }
+</script>
